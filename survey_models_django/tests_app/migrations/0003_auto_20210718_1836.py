21282f3e0c500599d8bf36a87f0004755a9171d3
DenMaslov <20denismaslov@gmail.com>
2021-08-10 01:06:48 +0300

    small update

--- survey_models_django/tests_app/migrations/0003_auto_20210718_1836.py
+++ survey_models_django/tests_app/migrations/0003_auto_20210718_1836.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tests', '0002_testrun_points'),
+        ('tests_app', '0002_testrun_points'),
     ]
 
     operations = [
