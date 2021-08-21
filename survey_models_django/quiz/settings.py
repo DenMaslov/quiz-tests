ac3f7a11d58506f10df44e2baf02db2497fd86ce
DenMaslov <76794599+DenMaslov@users.noreply.github.com>
2021-08-21 03:09:40 +0300

    Update settings.py

--- survey_models_django/quiz/settings.py
+++ survey_models_django/quiz/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qu6e7a-c$c+@#_#%mfcz6e7o+tijz7c)yl2l9ygd+fx(t8db*u'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
